31ebed88accd525a15b95bea855f14358ff62167
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-06-09 10:35:47 +0200

    pdf add as image done

--- src/main/java/com/github/funthomas424242/pdf2pocketmod/PDF2Pocketmod.java
+++ src/main/java/com/github/funthomas424242/pdf2pocketmod/PDF2Pocketmod.java
@@ -26,27 +26,62 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PDF2Pocketmod {
 
     public static void main(String args[]) throws Exception {
-        //Loading an existing document
-        File file = new File("C:/PdfBox_Examples/sample.pdf");
-        PDDocument doc = PDDocument.load(file);
+        final PDF2Pocketmod app = new PDF2Pocketmod();
+        app.run();
+    }
 
-        //Retrieving the page
-        PDPage page = doc.getPage(0);
+    public void run() throws IOException {
+        final PDDocument pocketmodPdf = new PDDocument();
+        final PDPage page = new PDPage();
+        pocketmodPdf.addPage( page );
+
+        final PDDocument doc1 = loadPage( Paths.get(".","/src/test/java/com/github/funthomas424242/pdf2pocketmod/"), "Seite", "pdf", 1);
+
+        //Instantiating the PDFRenderer class
+        final PDFRenderer renderer = new PDFRenderer(doc1);
+        //Rendering an image from the PDF document
+        final BufferedImage image = renderer.renderImage(0);
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", baos);
+        baos.flush();
+        byte[] bytes = baos.toByteArray();
+
+        //Writing the image to a file
+//        ImageIO.write(image, "JPEG", new File("/src/test/java/com/github/funthomas424242/pdf2pocketmod/Seite1.png"));
+
+
+        PDImageXObject pdImage = PDImageXObject.createFromByteArray(pocketmodPdf, bytes, "myImage Seite1");
+//        PDImageXObject pdImage = PDImageXObject.createFromFile("/src/test/java/com/github/funthomas424242/pdf2pocketmod/Seite1.png", doc);
+
+
+
+        System.out.println("Image created");
+
+//        pocketmodPdf.addPage(page1);
+
 
         //Creating PDImageXObject object
-        PDImageXObject pdImage = PDImageXObject.createFromFile("C:/PdfBox_Examples/logo.png", doc);
+//        PDImageXObject pdImage = PDImageXObject.createFromFile("C:/PdfBox_Examples/logo.png", doc);
 
         //creating the PDPageContentStream object
-        PDPageContentStream contents = new PDPageContentStream(doc, page);
+        PDPageContentStream contents = new PDPageContentStream(pocketmodPdf, page);
 
         //Drawing the image in the PDF document
-        contents.drawImage(pdImage, 70, 250);
+        contents.drawImage(pdImage, 1, 1);
+//        contents.drawImage(pdImage, 70, 250);
 
         System.out.println("Image inserted");
 
@@ -54,11 +89,19 @@ public class PDF2Pocketmod {
         contents.close();
 
         //Saving the document
-        doc.save("C:/PdfBox_Examples/sample.pdf");
+        pocketmodPdf.save("Pocketmod.pdf");
 
         //Closing the document
-        doc.close();
+        pocketmodPdf.close();
 
+    }
+
+
+    protected PDDocument loadPage(final Path folder, String filePrefix, String fileExtension, int pageNum) throws IOException {
+        //Loading page x
+        final File seite1 = Paths.get(folder.toAbsolutePath().toString(), filePrefix+pageNum+"."+fileExtension).toFile();
+        System.out.println(seite1.getAbsolutePath());
+        return PDDocument.load(seite1);
     }
 }
 
